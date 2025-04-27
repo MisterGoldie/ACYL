7427814cd7ca36f2a199b7f4bc6f23c672bacd88
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-04-27 13:10:12 -0500

    fixed header on website

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    allowedHosts: ["http://localhost:3000/"],
+    allowedHosts: ["504f92f5f563.ngrok.app"],
   },
 });
