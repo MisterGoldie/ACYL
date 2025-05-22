b086cdfccb24ef6fe0d26c53d2011f812ff3590a
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-05-22 12:30:08 -0500

    working on spacing on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -104,7 +104,7 @@ const HomePage = () => {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.3 }}
-        style={{ minHeight: '100vh', height: '100%' }}
+        style={{ minHeight: '100vh', height: 'auto' }}
       >
         <Header />
         <HomeContent />
